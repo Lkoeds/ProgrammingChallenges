4605516da29af0f479d86907990d06a6af1f7bae
Lkoeds <72240213+Lkoeds@users.noreply.github.com>
2020-12-26 19:04:57 -0500

    Update CountingCap6.java

--- CountingCap6.java
+++ CountingCap6.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package programmingchallenges;
 import java.math.BigInteger;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -11,7 +10,7 @@ import java.util.ArrayList;
  *
  * @author Admin
  */
-public class CountingCap6 {
+public class Main {
 
     static ArrayList<BigInteger> arr = new ArrayList<>();
     public static void main(String[] args) {
